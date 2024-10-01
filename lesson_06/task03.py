543e3a111867f4988373176d486206015f0d0db7
vadimduzh <Vadim.duzh@gmail.com>
2024-10-01 21:21:30 +0300

    added

---
+++ lesson_06/task03.py
@@ -0,0 +1,12 @@
+import random
+
+list_1 = []
+for i in range(31):
+    random_num = random.randint(-5, 10)
+    list_1.append(random_num)
+
+print(list_1)
+for l in list_1:
+    if l < 0:
+        print(list_1.index(l) + 1)
+        break
